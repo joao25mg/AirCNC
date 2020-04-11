62303fda913022f217e4edcf0c8b567221f8c762
João Paulo Oliveira Martins <joao.paulo-martins@hotmail.com>
2020-04-11 14:17:04 -0300

    backend finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello word!" });
-});
+mongoose.connect('mongodb+srv://joao:25031993@aircnc-mebbr.mongodb.net/bancodados?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//GET, POST, PUT, DELETE
+
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar rout params (para edição, delete)
+//req.body = Acessar corpo da requisição (para criação,edição)
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
