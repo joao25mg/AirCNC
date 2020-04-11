8379d4bd2ed4babc3da0ea7d986e768c64825c7f
João Paulo Oliveira Martins <joao.paulo-martins@hotmail.com>
2020-04-11 17:01:03 -0300

    implementação das rotas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://joao:25031993@aircnc-mebbr.mongodb.net/bancodado
 //req.params = Acessar rout params (para edição, delete)
 //req.body = Acessar corpo da requisição (para criação,edição)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
