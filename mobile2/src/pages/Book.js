ffc5ac30843cfbe19f1c3d5f7bfab13fb950508c
João Paulo Oliveira Martins <joao.paulo-martins@hotmail.com>
2020-04-13 20:06:57 -0300

    subir git

---
+++ mobile2/src/pages/Book.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import api from '../services/api';
+
+export default function Book() {
+    return (
+        <View />
+    );
+}
