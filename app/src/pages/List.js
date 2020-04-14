27fa502309bf0f5fe69cf5db5eb81aa9f1c9fec9
João Paulo Oliveira Martins <joao.paulo-martins@hotmail.com>
2020-04-13 23:26:21 -0300

    Lista de tecnologias

--- app/src/pages/List.js
+++ app/src/pages/List.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
 
-import api from '../services/api';
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    //Node.js, ReactJS
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+    
     return (
-        <View />
-    );
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+         flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: 'center',
+        marginTop: 40
+
+    },
+});
